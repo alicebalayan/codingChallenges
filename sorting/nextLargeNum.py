290e044f76f727f2814770596bf3ceeee310bc61
Alice Balayan <balayan.alice@gmail.com>
2019-08-12 08:59:38 -0700

    added sorting challenges

---
+++ sorting/nextLargeNum.py
@@ -0,0 +1,10 @@
+'''
+Problem: 
+
+Find the next largest number that can be created from the same numbers as the input. 
+
+From: https://edabit.com/challenge/6b8Px5m82nAgudhsy
+
+'''
+
+def next_number(num):
