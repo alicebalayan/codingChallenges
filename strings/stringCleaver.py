71fa6380fc64838f58e9783ff65393fa8ccf424b
Alice Balayan <balayan.alice@gmail.com>
2019-08-12 08:53:20 -0700

    added string challenges

---
+++ strings/stringCleaver.py
@@ -0,0 +1,8 @@
+'''
+Problem: 
+Create a function that takes a string (without spaces) and a word list, cleaves the string into words based on the list, and returns the correctly spaced version of the string (a sentence).
+If a section of the string is encountered that can't be found on the word list, return "Cleaving stalled: Word not found"
+
+From: https://edabit.com/challenge/FWh2fGH7aRWALMf3o
+'''
+def cleave(string, lst):
