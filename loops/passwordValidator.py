4d1c3546cf3ae03b6fe943ba8381e0d60b5263e7
Alice Balayan <balayan.alice@gmail.com>
2019-08-12 09:02:13 -0700

    added loops challenges

---
+++ loops/passwordValidator.py
@@ -0,0 +1,6 @@
+'''
+Write a Python program to check the validity of password input by users.
+
+From (#15): https://www.w3resource.com/python-exercises/python-conditional-statements-and-loop-exercises.php
+
+'''
