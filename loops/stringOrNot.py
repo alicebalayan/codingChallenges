8401fc3c0343a4860385bf1298de92cab7550580
Alice Balayan <balayan.alice@gmail.com>
2019-08-12 09:04:03 -0700

    added stringOrNot.py in /loops

---
+++ loops/stringOrNot.py
@@ -0,0 +1,6 @@
+'''
+Write a Python program to check a string represent an integer or not?
+
+From (#35): https://www.w3resource.com/python-exercises/python-conditional-statements-and-loop-exercises.php
+
+'''
