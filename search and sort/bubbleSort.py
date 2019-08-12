ee97cafdd3561f600d3546b613a96ad71c52476d
Alice Balayan <balayan.alice@gmail.com>
2019-08-12 09:07:01 -0700

    added bubbleSort.py in /search and sort

---
+++ search and sort/bubbleSort.py
@@ -0,0 +1,6 @@
+'''
+Write a Python program to sort a list of elements using the bubble sort algorithm.
+
+From (#4): https://www.w3resource.com/python-exercises/data-structures-and-algorithms/index.php
+
+'''
