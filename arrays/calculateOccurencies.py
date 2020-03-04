f7c221f9b3b1c15c409c320b82d60392ed67823f
Alice Balayan <balayan.alice@gmail.com>
2020-03-03 20:38:47 -0800

    Add solution for array problem.

--- arrays/calculateOccurencies.py
+++ arrays/calculateOccurencies.py
@@ -4,3 +4,15 @@ Write a Python program to get the number of occurrences of a specified element i
 
 From (#6): https://www.w3resource.com/python-exercises/array/ 
 '''
+
+a1 = [11, 23, 50, 23, 100, 100, 100]
+specified1 = 100
+
+def occurence(input_array, target):
+    count = 0
+    for i in range(len(input_array)):
+        if target == input_array[i]:
+            count += 1
+    return count
+
+print(occurence(a1, specified1))
